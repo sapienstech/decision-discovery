9d85458ba6239c967929370bb0b2c601253ed0f9
Roni Frantchi <roni.frantchi@gmail.com>
2015-12-21 04:49:06 +0200

    Initial commit

---
+++ src/main/java/com/decision/cloud/DecisionDiscoveryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.decision.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DecisionDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecisionDiscoveryServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/decision/cloud/DecisionDiscoveryServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.decision.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DecisionDiscoveryServiceApplication.class)
+public class DecisionDiscoveryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
